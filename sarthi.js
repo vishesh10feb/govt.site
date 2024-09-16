c858445e8dbb11ea646404e34f183f1db394be00
vishesh10feb <152710676+vishesh10feb@users.noreply.github.com>
2024-09-16 11:06:45 +0530

    Add files via upload

---
+++ sarthi.js
@@ -0,0 +1,11 @@
+document.getElementById('showChatbotBtn').addEventListener('click', function() {
+    document.getElementById('chatbot').style.display = 'block';
+});
+
+document.getElementById('closeChatbotBtn').addEventListener('click', function() {
+    document.getElementById('chatbot').style.display = 'none';
+});
+
+
+
+
